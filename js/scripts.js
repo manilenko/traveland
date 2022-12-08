4d8b8ea6295d894a2410ca3db79c0ed19fa0d0d5
JuliaAnat <107546189+JuliaAnat@users.noreply.github.com>
2022-12-07 22:20:16 -0500

    changes have been made in swiper slide

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,31 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
    },
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
 
-   // And if we need scrollbar
-   scrollbar: {
-      el: '.swiper-scrollbar',
    },
+
+   slidesPerView: 4,
+   spaceBetween: 60,
+   // Responsive breakpoints
+   breakpoints: {
+      // when window width is >= 320px
+      320: {
+         slidesPerView: 2,
+         spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+         slidesPerView: 3,
+         spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+         slidesPerView: 4,
+         spaceBetween: 40
+      }
+   }
 });
